347f4ae71576a5dd25f41d5b7e7b2e0646ce3489
ABaddlesmere <alexdickers@gmail.com>
2022-11-22 11:22:17 +0000

    Add from403 and cleanup

--- mypal/client/http/limiter.py
+++ mypal/client/http/limiter.py
@@ -4,28 +4,41 @@ class Limiter:
     A class to handle rate limiting for the router
     '''
     def __init__(self, limit: int = 10) -> None:
-        self.max = limit
-        self.history = []
+        self.__max = limit
+        self.__history = []
 
     def lockCheck(self):
-        print(f"self.history is {self.history}")
-        if len(self.history) < self.max:
-            self.history.append(time.time())
+        current_time = time.time()
+        print(f"self.__history is {self.__history}")
+        if len(self.__history) < self.__max:
+            self.__history.append(current_time)
             print(f"passed")
             return False
         else:
-            if (time.time()-min(self.history)) <= 1:
-                print(f"failed, time diff is {time.time()-min(self.history)}")
+            if (current_time-min(self.__history)) <= 1:
+                print(f"failed, time diff is {current_time-min(self.__history)}")
                 return True
             else:
-                self.history = []
-                self.history.append(time.time())
+                self.__history = []
+                self.__history.append(current_time)
                 print(f"history reset | passed")
                 return False
 
     def unlockCheck(self):
-        if (time.time()-min(self.history)) >= 60:
-            self.history = []
+        if (time.time()-max(self.__history)) >= 60:
+            self.__history = []
             return True
         else:
             return False
+
+    def checkFrom403(self):
+        '''
+        If last 5 are all within 1s, assume MAL is rate limiting us!
+        Otherwise, they are protecting from possible DOS
+        '''
+        violators = 0
+        current_time = time.time()
+        for i in range(-1, -len(self.__history), -1):
+            if current_time-self.__history[:i] <= 1:
+                violators += 1
+        return violators >= 5
