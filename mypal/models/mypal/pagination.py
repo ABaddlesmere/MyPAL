3938542f4dcfe9fd0ff955e9bf954f517a9b92e3
ABaddlesmere <alexdickers@gmail.com>
2022-12-01 20:29:09 +0000

    Turn pagination nodes into properties
    This can be useful if limit is 1, since a pagination is always returned in some aspects

--- mypal/models/mypal/pagination.py
+++ mypal/models/mypal/pagination.py
@@ -7,11 +7,25 @@ class Pagination:
     def __init__(self, data: dict, NodeType: Anime|Manga, fields: list[AnimeField]|list[MangaField]|AnimeField|MangaField):
         self.next: str = data['paging'].get("next", None)
         self.previous: str = data['paging'].get("previous", None)
-        self.nodes: list[Media] = []
+        self.__nodes: list[Media] = []
 
         #Paginate nodes
         for node in data['data']:
             self.nodes.append(NodeType(node['node'], fields))
+
+    @property
+    def node(self):
+        '''
+        Returns the first node in the pagination as its node type
+        '''
+        return self.__nodes[0]
+    
+    @property
+    def nodes(self):
+        '''
+        Returns all nodes in the pagination as a list
+        '''
+        return self.__nodes
 
     def __repr__(self) -> str:
         return f"Pagination object with {len(self.nodes)} nodes ; next URL: {self.next} ; previous URL: {self.previous}"
